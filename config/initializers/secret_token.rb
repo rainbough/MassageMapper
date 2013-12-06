fdfabdeb15391a555791150450e0dc1ad4cd3a79
Rainbough <rainbough@gmail.com>
2013-12-06 13:38:05 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MassageMap::Application.config.secret_key_base = '104f3bf5058758f15c2d166891b1e4ffc608040f2237bc8614f190f1e85dc2072a2089cefb0c452017e40b33e9df7f760a7446b58058bbcd3336145bf33bd6e1'
